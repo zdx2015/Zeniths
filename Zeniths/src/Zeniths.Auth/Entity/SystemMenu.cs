540edece2194f234cb652668d43e87efeeeaa967
吕艳阳 <lvyanyang@hotmail.com>
2015-10-08 17:34:30 +0800

    ddd

---
+++ Zeniths/src/Zeniths.Auth/Entity/SystemMenu.cs
@@ -0,0 +1,155 @@
+﻿// ===============================================================================
+//  Copyright (c) 2015 正得信集团股份有限公司
+// ===============================================================================
+
+using System;
+using Zeniths.Entity;
+
+namespace Zeniths.Auth.Entity
+{
+    /// <summary>
+    /// 系统菜单
+    /// </summary>
+    [Table(Caption = "系统菜单")]
+    [PrimaryKey("Id", true)]
+    [ParentKey("ParentId")]
+    [SortPath("SortPath")]
+    public class SystemMenu
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        //[Required(ErrorMessage = "请输入主键")]
+        //[StringLength(4, ErrorMessage = "主键长度不能超过{1}")]
+        [Column(Caption = "主键")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 父级主键
+        /// </summary>
+        //[Required(ErrorMessage = "请输入父级主键")]
+        //[StringLength(4, ErrorMessage = "父级主键长度不能超过{1}")]
+        [Column(Caption = "父级主键")]
+        public int ParentId { get; set; }
+
+        /// <summary>
+        /// 编码
+        /// </summary>
+        //[Required(ErrorMessage = "请输入编码")]
+        //[StringLength(100, ErrorMessage = "编码长度不能超过{1}")]
+        [Column(Caption = "编码")]
+        public string Code { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        //[Required(ErrorMessage = "请输入名称")]
+        //[StringLength(100, ErrorMessage = "名称长度不能超过{1}")]
+        [Column(Caption = "名称")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 简拼
+        /// </summary>
+        //[StringLength(100, ErrorMessage = "简拼长度不能超过{1}")]
+        [Column(Caption = "简拼")]
+        public string NameSpell { get; set; }
+
+        /// <summary>
+        /// 排序路径
+        /// </summary>
+        //[Required(ErrorMessage = "请输入排序路径")]
+        //[StringLength(1000, ErrorMessage = "排序路径长度不能超过{1}")]
+        [Column(Caption = "排序路径")]
+        public string SortPath { get; set; }
+
+        /// <summary>
+        /// Web导航地址
+        /// </summary>
+        //[StringLength(500, ErrorMessage = "Web导航地址长度不能超过{1}")]
+        [Column(Caption = "Web导航地址")]
+        public string WebUrl { get; set; }
+
+        /// <summary>
+        /// Web大图标样式
+        /// </summary>
+        //[StringLength(100, ErrorMessage = "Web大图标样式长度不能超过{1}")]
+        [Column(Caption = "Web大图标样式")]
+        public string WebCls { get; set; }
+
+        /// <summary>
+        /// Web打开方式
+        /// </summary>
+        //[StringLength(100, ErrorMessage = "Web打开方式长度不能超过{1}")]
+        [Column(Caption = "Web打开方式")]
+        public string WebOpenMode { get; set; }
+
+        /// <summary>
+        /// Win窗口类名
+        /// </summary>
+        //[StringLength(500, ErrorMessage = "Win窗口类名长度不能超过{1}")]
+        [Column(Caption = "Win窗口类名")]
+        public string WinFormProvider { get; set; }
+
+        /// <summary>
+        /// Win对话框显示
+        /// </summary>
+        //[Required(ErrorMessage = "请输入Win对话框显示")]
+        //[StringLength(1, ErrorMessage = "Win对话框显示长度不能超过{1}")]
+        [Column(Caption = "Win对话框显示")]
+        public bool WinShowDialog { get; set; }
+
+        /// <summary>
+        /// Win大图标名称
+        /// </summary>
+        //[StringLength(100, ErrorMessage = "Win大图标名称长度不能超过{1}")]
+        [Column(Caption = "Win大图标名称")]
+        public string WinLargeIconName { get; set; }
+
+        /// <summary>
+        /// Win小图标名称
+        /// </summary>
+        //[StringLength(100, ErrorMessage = "Win小图标名称长度不能超过{1}")]
+        [Column(Caption = "Win小图标名称")]
+        public string WinSmallIconName { get; set; }
+
+        /// <summary>
+        /// 是否展开
+        /// </summary>
+        //[Required(ErrorMessage = "请输入是否展开")]
+        //[StringLength(1, ErrorMessage = "是否展开长度不能超过{1}")]
+        [Column(Caption = "是否展开")]
+        public bool IsExpand { get; set; }
+
+        /// <summary>
+        /// 是否公开
+        /// </summary>
+        //[Required(ErrorMessage = "请输入是否公开")]
+        //[StringLength(1, ErrorMessage = "是否公开长度不能超过{1}")]
+        [Column(Caption = "是否公开")]
+        public bool IsPublic { get; set; }
+
+        /// <summary>
+        /// 是否启用
+        /// </summary>
+        //[Required(ErrorMessage = "请输入是否启用")]
+        //[StringLength(1, ErrorMessage = "是否启用长度不能超过{1}")]
+        [Column(Caption = "是否启用")]
+        public bool IsEnabled { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        //[StringLength(1000, ErrorMessage = "备注长度不能超过{1}")]
+        [Column(Caption = "备注")]
+        public string Note { get; set; }
+
+        /// <summary>
+        /// 复制对象
+        /// </summary>
+        public SystemMenu Clone()
+        {
+            return (SystemMenu)this.MemberwiseClone();
+        }
+    }
+}
---
+++ Zeniths/src/Zeniths.Auth/Service/SystemMenuService.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Zeniths.Auth.Entity;
+using Zeniths.Collections;
+using Zeniths.Data;
+using Zeniths.Extensions;
+using Zeniths.Utility;
+
+namespace Zeniths.Auth.Service
+{
+    public class SystemMenuService
+    {
+        /// <summary>
+        /// 存储器
+        /// </summary>
+        private readonly AuthRepository<SystemMenu> repos = new AuthRepository<SystemMenu>();
+
+        /// <summary>
+        /// 检测是否存在指定系统菜单
+        /// </summary>
+        /// <param name="entity">系统菜单实体</param>
+        /// <returns>存在返回true</returns>
+        public bool Exists(SystemMenu entity)
+        {
+            return repos.Exists(p => p.Code == entity.Code && p.Id != entity.Id);
+        }
+
+        /// <summary>
+        /// 添加系统菜单
+        /// </summary>
+        /// <param name="entity">系统菜单实体</param>
+        public BoolMessage Insert(SystemMenu entity)
+        {
+            try
+            {
+                repos.Insert(entity);
+                return BoolMessage.True;
+            }
+            catch (Exception e)
+            {
+                return new BoolMessage(false, e.Message);
+            }
+        }
+
+        /// <summary>
+        /// 更新系统菜单
+        /// </summary>
+        /// <param name="entity">系统菜单实体</param>
+        public BoolMessage Update(SystemMenu entity)
+        {
+            try
+            {
+                repos.Update(entity);
+                return BoolMessage.True;
+            }
+            catch (Exception e)
+            {
+                return new BoolMessage(false, e.Message);
+            }
+        }
+
+        /// <summary>
+        /// 删除系统菜单
+        /// </summary>
+        /// <param name="ids">系统菜单主键数组</param>
+        public BoolMessage Delete(int[] ids)
+        {
+            try
+            {
+                if (ids.Length == 1)
+                {
+                    repos.Delete(ids[0]);
+                }
+                else
+                {
+                    repos.Delete(ids);
+                }
+                return BoolMessage.True;
+            }
+            catch (Exception e)
+            {
+                return new BoolMessage(false, e.Message);
+            }
+        }
+
+        /// <summary>
+        /// 获取系统菜单对象
+        /// </summary>
+        /// <param name="id">系统菜单主键</param>
+        /// <returns>系统菜单对象</returns>
+        public SystemMenu Get(int id)
+        {
+            return repos.Get(id);
+        }
+
+        /// <summary>
+        /// 获取系统菜单列表
+        /// </summary>
+        /// <returns>系统菜单列表</returns>
+        public List<SystemMenu> GetList()
+        {
+            var query = repos.NewQuery.OrderBy(p=>p.SortPath);
+            return repos.Query(query).ToList();
+        }
+
+        /// <summary>
+        /// 获取启用模块列表
+        /// </summary>
+        public List<SystemMenu> GetEnabledList()
+        {
+            var query = repos.NewQuery.OrderBy(p => p.SortPath)
+                .Where(p=>p.IsEnabled == true);
+            return repos.Query(query).ToList();
+        }
+    }
+}
