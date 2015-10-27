7596cc85b6151aebd153ec6854e81f899a3df715
506555768 <506555768@qq.com>
2015-10-27 11:15:42 +0800

    部门预算实体

---
+++ Zeniths/src/Zeniths.Hr/Entity/HrBudget.cs
@@ -0,0 +1,156 @@
+﻿// ===============================================================================
+// 正得信股份 版权所有
+// ===============================================================================
+
+using System;
+using Zeniths.Entity;
+
+namespace Zeniths.Hr.Entity
+{
+    /// <summary>
+    /// 部门预算
+    /// </summary>
+    [Table(Caption = "部门预算")]
+    public class HrBudget
+    {
+		/// <summary>
+        /// 主键
+        /// </summary>
+		[Column(Caption = "主键")]
+        public int Id { get; set; }
+
+		/// <summary>
+        /// 任务标题
+        /// </summary>
+		[Column(Caption = "任务标题")]
+        public string Title { get; set; }
+
+		/// <summary>
+        /// 预算月份
+        /// </summary>
+		[Column(Caption = "预算月份")]
+        public DateTime BudgetMonth { get; set; }
+
+		/// <summary>
+        /// 预算部门主键
+        /// </summary>
+		[Column(Caption = "预算部门主键")]
+        public int BudgetDepartmentId { get; set; }
+
+		/// <summary>
+        /// 预算部门名称
+        /// </summary>
+		[Column(Caption = "预算部门名称")]
+        public string BudgetDepartmentName { get; set; }
+
+		/// <summary>
+        /// 流程主键
+        /// </summary>
+		[Column(Caption = "流程主键")]
+        public int? FlowId { get; set; }
+
+		/// <summary>
+        /// 流程名称
+        /// </summary>
+		[Column(Caption = "流程名称")]
+        public string FlowName { get; set; }
+
+		/// <summary>
+        /// 流程实例主键
+        /// </summary>
+		[Column(Caption = "流程实例主键")]
+        public int? FlowInstanceId { get; set; }
+
+		/// <summary>
+        /// 当前步骤主键
+        /// </summary>
+		[Column(Caption = "当前步骤主键")]
+        public int? StepId { get; set; }
+
+		/// <summary>
+        /// 当前步骤名称
+        /// </summary>
+		[Column(Caption = "当前步骤名称")]
+        public string StepName { get; set; }
+
+		/// <summary>
+        /// 总经理主键
+        /// </summary>
+		[Column(Caption = "总经理主键")]
+        public int? GeneralManagerId { get; set; }
+
+		/// <summary>
+        /// 总经理名称
+        /// </summary>
+		[Column(Caption = "总经理名称")]
+        public string GeneralManagerName { get; set; }
+
+		/// <summary>
+        /// 总经理审批状态
+        /// </summary>
+		[Column(Caption = "总经理审批状态")]
+        public bool? GeneralManagerStatus { get; set; }
+
+		/// <summary>
+        /// 总经理审批意见
+        /// </summary>
+		[Column(Caption = "总经理审批意见")]
+        public string GeneralManagerNote { get; set; }
+
+		/// <summary>
+        /// 总经理审批时间
+        /// </summary>
+		[Column(Caption = "总经理审批时间")]
+        public DateTime? GeneralManagerDate { get; set; }
+
+		/// <summary>
+        /// 处理状态
+        /// </summary>
+		[Column(Caption = "处理状态")]
+        public int? Status { get; set; }
+
+		/// <summary>
+        /// 流程是否完成
+        /// </summary>
+		[Column(Caption = "流程是否完成")]
+        public string IsFinish { get; set; }
+
+		/// <summary>
+        /// 创建用户主键
+        /// </summary>
+		[Column(Caption = "创建用户主键")]
+        public int CreateUserId { get; set; }
+
+		/// <summary>
+        /// 创建用户姓名
+        /// </summary>
+		[Column(Caption = "创建用户姓名")]
+        public string CreateUserName { get; set; }
+
+		/// <summary>
+        /// 创建用户部门主键
+        /// </summary>
+		[Column(Caption = "创建用户部门主键")]
+        public int CreateDepartmentid { get; set; }
+
+		/// <summary>
+        /// 创建用户部门名称
+        /// </summary>
+		[Column(Caption = "创建用户部门名称")]
+        public string CreateDepartmentName { get; set; }
+
+		/// <summary>
+        /// 创建时间
+        /// </summary>
+		[Column(Caption = "创建时间")]
+        public DateTime CreateDateTime { get; set; }
+
+        /// <summary>
+        /// 复制对象
+        /// </summary>
+        public HrBudget Clone()
+        {
+            return (HrBudget)this.MemberwiseClone();
+        }
+    }
+}
---
+++ Zeniths/src/Zeniths.Hr/Entity/HrBudgetDetails.cs
@@ -0,0 +1,66 @@
+﻿// ===============================================================================
+// 正得信股份 版权所有
+// ===============================================================================
+
+using System;
+using Zeniths.Entity;
+
+namespace Zeniths.Hr.Entity
+{
+    /// <summary>
+    /// 预算明细信息
+    /// </summary>
+    [Table(Caption = "预算明细信息")]
+    public class HrBudgetDetails
+    {
+		/// <summary>
+        /// 主键
+        /// </summary>
+		[Column(Caption = "主键")]
+        public int Id { get; set; }
+
+		/// <summary>
+        /// 预算主键
+        /// </summary>
+		[Column(Caption = "预算主键")]
+        public int BudgetId { get; set; }
+
+		/// <summary>
+        /// 预算类别主键
+        /// </summary>
+		[Column(Caption = "预算类别主键")]
+        public int BudgetCategoryId { get; set; }
+
+		/// <summary>
+        /// 预算类别名称
+        /// </summary>
+		[Column(Caption = "预算类别名称")]
+        public string BudgetCategoryName { get; set; }
+
+		/// <summary>
+        /// 项目名称
+        /// </summary>
+		[Column(Caption = "项目名称")]
+        public string BudgetItemName { get; set; }
+
+		/// <summary>
+        /// 预算金额
+        /// </summary>
+		[Column(Caption = "预算金额")]
+        public decimal BudgetMoney { get; set; }
+
+		/// <summary>
+        /// 备注
+        /// </summary>
+		[Column(Caption = "备注")]
+        public string Note { get; set; }
+
+        /// <summary>
+        /// 复制对象
+        /// </summary>
+        public HrBudgetDetails Clone()
+        {
+            return (HrBudgetDetails)this.MemberwiseClone();
+        }
+    }
+}
