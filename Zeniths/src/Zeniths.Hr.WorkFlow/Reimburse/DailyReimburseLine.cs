0019302f6e61694e97f1c1a9cccf3f3ffa3918b3
18chua <0298384213@163.com>
2015-11-02 15:53:33 +0800

    123123

--- Zeniths/src/Zeniths.Hr.WorkFlow/Reimburse/DailyReimburseLine.cs
+++ Zeniths/src/Zeniths.Hr.WorkFlow/Reimburse/DailyReimburseLine.cs
@@ -34,4 +34,28 @@ namespace Zeniths.Hr.WorkFlow.Reimburse
         }
     }
 
+    [WorkFlowEventCaption("日常报销:报销大于等于5万")]
+    public class DailyReimburseMoreThan50000Line : DefaultLineEvent
+    {
+        public override BoolMessage OnValid(FlowLineEventArgs args)
+        {
+            var result = new BoolMessage(true);
+            //var service = new DailyReimburseDetailsService();
+            //result = service.IsInnerBudget(args.BusinessId.ToInt(), args.CurrentUser.DepartmentId);
+            return result;
+        }
+    }
+
+    [WorkFlowEventCaption("日常报销:报销小于5万")]
+    public class DailyReimburseLessThan50000Line : DefaultLineEvent
+    {
+        public override BoolMessage OnValid(FlowLineEventArgs args)
+        {
+            var result = new BoolMessage(true);
+            //var service = new DailyReimburseDetailsService();
+            //result = service.IsInnerBudget(args.BusinessId.ToInt(), args.CurrentUser.DepartmentId);
+            return result;
+        }
+    }
+
 }
